196a8ab759a6c1d0115b3de8b153f60abce26601
Carlos Rodriguez <carlos@s8f.org>
2016-07-27 16:12:18 -0700

    fix

--- examples/zen_ebooks/utils/sanitize_tweet_text.js
+++ examples/zen_ebooks/utils/sanitize_tweet_text.js
@@ -1,10 +1,38 @@
+var not_proper = [
+  'Live',
+  'Fire',
+  'He',
+  'The',
+  'Hack',
+  'Think',
+  'Women',
+  'Born',
+  'Before',
+  'Could',
+  'Makes',
+  'Crooked',
+  'Black',
+  'Hat',
+  'Ruled',
+  'Valley'
+]
+var not_proper_regex = new RegExp(' (' + not_proper.join('|') + ') ', 'g')
+
 module.exports = function container (get, set, clear) {
   return function sanitize_tweet_text (text) {
     while (text.length > 135) {
       var words = text.split(/\s+/)
       text = words.slice(0, words.length - 1).join(' ')
     }
+    var quote_match = text.match(/\'|\"/g)
+    // strip unbalanced quotes
+    if (quote_match && quote_match.length % 2 !== 0) {
+      text = text.replace(/\'|\"/g, '')
+    }
+
     return text
+      // lowercase some words
+      .replace(not_proper_regex, function (match, idx, full) { return match.toLowerCase() })
       // strip retweet
       .replace(/RT @[^\s]+\: /g, '')
       // strip handles
