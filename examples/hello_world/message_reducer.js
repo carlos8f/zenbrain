a888ab3ea8c8bbace3c76da49503e6fac2b45d31
Carlos Rodriguez <carlos@s8f.org>
2016-08-10 22:51:30 -0700

    wip

---
+++ examples/hello_world/message_reducer.js
@@ -0,0 +1,16 @@
+module.exports = function container (get, set, clear) {
+  return function thought_reducer (g, cb) {
+    var tick = g.tick, thoughts = g.thoughts
+    // create a data key
+    tick.data.messages = []
+    // filter thoughts by key = message
+    thoughts.forEach(function (thought) {
+      if (thought.key !== 'message') {
+        return
+      }
+      // migrate message text to the data key
+      tick.data.messages.push(thought.value.text)
+    })
+    cb(null, g)
+  }
+}
