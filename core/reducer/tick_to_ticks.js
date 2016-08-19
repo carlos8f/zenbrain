999dec0eaf53697cf312112b251d78dbe942f6ec
Carlos Rodriguez <carlos@s8f.org>
2016-08-18 23:18:29 -0700

    perf report options

--- core/reducer/tick_to_ticks.js
+++ core/reducer/tick_to_ticks.js
@@ -9,16 +9,18 @@ module.exports = function container (get, set, clear) {
   var passive_update = get('utils.passive_update')
   var counter = 0
   var per_sec = 0
-  setInterval(function () {
+  function reducer_perf_report () {
     per_sec = n(counter).divide(30).format('0')
-    if (per_sec !== '0') {
-      get('db').collection('thoughts').count({app: get('app_name')}, function (err, thought_count) {
-        if (err) throw err
+    get('db').collection('thoughts').count({app: get('app_name')}, function (err, thought_count) {
+      if (err) throw err
+      if (n(counter).divide(30).value() >= c.reducer_perf_report_min || thought_count) {
         get('logger').info('reducer', 'processing  '.grey + per_sec + '/ticks sec, thought queue: '.grey + thought_count, {feed: 'reducer'})
-      })
-    }
-    counter = 0
-  }, 30000)
+      }
+      counter = 0
+      setTimeout(reducer_perf_report, c.reducer_perf_report_timeout)
+    })
+  }
+  setTimeout(reducer_perf_report, c.reducer_perf_report_timeout)
   return function tick_to_ticks (sub_tick) {
     //get('logger').info('tick_to_ticks', sub_tick.id)
     counter++
