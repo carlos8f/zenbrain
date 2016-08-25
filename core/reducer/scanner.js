b2ef9df43e33d05baa10bb337938bc68b94d19f9
Carlos Rodriguez <carlos@s8f.org>
2016-08-25 16:16:01 -0700

    scan note

--- core/reducer/scanner.js
+++ core/reducer/scanner.js
@@ -1,1 +1,14 @@
-scanner.js
+for each reducer size,
+find first tick
+create timebucket in that size
+do calc on tick
+walk into the future
+do calc on each tick
+stop at the first gap
+check for ticks > gap
+
+if ticks,
+fill in gap with placeholder ticks
+
+for each reducer size,
+find first tick
