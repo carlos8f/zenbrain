198faaa80c62c773ffd59ee481a0b66371bab2a1
Carlos Rodriguez <carlos@s8f.org>
2016-08-26 01:44:49 -0700

    wip scanner

--- core/reducer/scanner.js
+++ core/reducer/scanner.js
@@ -41,15 +41,19 @@ module.exports = function container (get, set, clear) {
           // set up an env with
           // - `tick` being scanned
           // - `last_tick` scanned
-          s.first_time = unprocessed_start.pop().time
-          s.bucket = tb(s.first_time).resize(size)
-          get('logger').info('scanner', get_tick_str(s.bucket.toString()), 'first tick'.grey)
+          var start_tick = unprocessed_start.pop()
+          s.first_time = s.first_time ? Math.min(s.first_time, start_tick.time) : start_tick.time
+          s.scan_time = start_tick.time
+          var first_tick_id = get('app_name') + ':' + tb(s.first_time).resize(size).toString()
+          //get('logger').info('scanner', get_tick_str(first_tick_id), 'first tick'.grey)
+          //get('logger').info('scanner', get_tick_str(start_tick.id), 'scan start'.grey)
           // scan back one to get one item of history.
+          var history_bucket = tb(s.scan_time).resize(size).subtract(1)
           get('ticks').select({
             query: {
               app: get('app_name'),
               size: size,
-              time: tb(s.bucket.toString()).subtract(1).toMilliseconds()
+              time: history_bucket.toMilliseconds()
             },
             limit: 1,
             sort: {
@@ -59,113 +63,115 @@ module.exports = function container (get, set, clear) {
             if (err) return done(err)
             with_history(history_ticks)
           })
-        })
-        function with_history (g, history_ticks) {
-          if (history_ticks.length) {
-            g.last_tick = history_ticks.pop()
-          }
-          else {
-            // invent a tick
-            g.last_tick = tick_defaults(tb(g.bucket.toString()).subtract(1).toString(), size)
-          }
-          get('logger').info('scanner', g.last_tick ? get_tick_str(g.last_tick.id) : 'null'.grey, 'last tick'.grey)
-          g.current_tick = g.first_tick
-          scan_tick(g, function (err, g) {
-            if (err) return done(err)
-            scan_forward(g)
-          })
-        }
-        function scan_tick (scan_done) {
-          // record original for 3-way merge
-          get('logger').info('scanner', get_tick_str(current_tick.id), 'scanning', {feed: 'scanner'})
-          var orig = JSON.parse(JSON.stringify(g.current_tick))
-          apply_funcs(g, get('scanners'), function (err, g) {
-            if (err) return scan_done(err)
-            var mine = g.current_tick
-            // update the processed flag
-            mine.processed = true
-            // defer the merge and continue
-            passive_update('ticks', mine.id, mine, function (tick, update_done) {
-              // merge passed-in tick with scan result
-              var diff = xdiff.diff3(mine, orig, tick)
-              var merged = xdiff.patch(orig, diff)
-              get('logger').info('scanner', get_tick_str(tick.id), 'scanned', merged, {feed: 'scanner'})
-              update_done(null, merged)
-            })
-            scan_done(null, g)
-          })
-        }
-        function scan_forward (g) {
-          // scan forward, filling in missing ticks
-          // run callbacks in sequence until we reach the present
-          var next_time = tb(g.current_tick.toString()).add(1).toMilliseconds()
-          ;(function scan (err) {
-            if (err) return done(err)
-            get('ticks').select({
-              query: {
-                app: get('app_name'),
-                size: size,
-                time: {
-                  $gt: g.current_tick.time
-                }
-              },
-              limit: c.scanner_limit,
-              sort: {
-                time: 1
-              }
-            }, function (err, scan_ticks) {
-              if (err) return done(err)
-              if (!scan_ticks.length) {
-                // fill in final gap to the present
-                var sub_tasks = [], now = new Date().getTime()
-                while (next_time <= now) {
-                  var next_id = tb(next_time).resize(size).toString()
-                  sub_tasks.push(function (sub_done) {
-                    // fill in missing ticks
-                    var missing_tick = tick_defaults(next_id, size)
-                    g.last_tick = g.current_tick
-                    g.current_tick = missing_tick
-                    scan_tick(g, sub_done)
-                  })
-                  g.bucket.add(1)
-                  next_time = g.bucket.toMilliseconds()
-                }
-                get('logger').info('scanner', 'end of ticks, synthesizing final'.grey, String(sub_tasks.length).grey, 'ticks.'.grey)
-                return series(sub_tasks, cb)
-              }
-              with_scan_ticks(scan_ticks)
-            })
-            function with_scan_ticks (scan_ticks) {
-              // bucket is incremented after each scan
-              // fill in missing ticks with default values
-              next_time = g.bucket.toMilliseconds()
-              var sub_tasks = []
-              scan_ticks.forEach(function (current_tick) {
-                while (next_time < current_tick.time) {
-                  var next_id = tb(next_time).resize(size).toString()
-                  sub_tasks.push(function (sub_done) {
-                    // fill in missing ticks
-                    var missing_tick = tick_defaults(next_id, size)
-                    g.last_tick = g.current_tick
-                    g.current_tick = missing_tick
-                    scan_tick(g, sub_done)
-                  })
-                  g.bucket.add(1)
-                  next_time = g.bucket.toMilliseconds()
-                }
-                sub_tasks.push(function (sub_done) {
-                  // scan tick
-                  g.last_tick = g.current_tick
-                  g.current_tick = current_tick
-                  scan_tick(g, sub_done)
-                })
-                g.bucket.add(1)
-                next_time = g.bucket.toMilliseconds()
-              })
-              series(sub_tasks, scan)
+          function with_history (history_ticks) {
+            var g = {}
+            if (history_ticks.length) {
+              // last tick from history query
+              g.last_tick = history_ticks.pop()
             }
-          })()
-        }
+            else {
+              // invent a tick
+              g.last_tick = tick_defaults(history_bucket.toString(), size)
+            }
+            //get('logger').info('scanner', get_tick_str(g.last_tick.id), 'last tick'.grey)
+            g.tick = start_tick
+            scan_tick(g, function (err, g) {
+              if (err) return done(err)
+              scan_forward(g)
+            })
+          }
+          function scan_tick (g, scan_done) {
+            // record original for 3-way merge
+            //get('logger').info('scanner', get_tick_str(g.tick.id), 'scanning', {feed: 'scanner'})
+            var orig = JSON.parse(JSON.stringify(g.tick))
+            apply_funcs(g, get('scanners'), function (err, g) {
+              if (err) return scan_done(err)
+              var mine = g.tick
+              // update the processed flag
+              mine.processed = true
+              // defer the merge and continue
+              passive_update('ticks', mine.id, mine, function (tick, update_done) {
+                // merge passed-in tick with scan result
+                var diff = xdiff.diff3(tick, orig, mine) || []
+                var merged = xdiff.patch(orig, diff)
+                update_done(null, merged)
+              }, function (err, tick) {
+                if (err) throw err
+                get('logger').info('scanner', get_tick_str(tick.id), 'scanned', tick, {feed: 'scanner'})
+              })
+              scan_done(null, g)
+            })
+          }
+          function scan_forward (g) {
+            // scan forward, filling in missing ticks
+            // run callbacks in sequence until we reach the present
+            ;(function scan (err) {
+              if (err) return done(err)
+              get('ticks').select({
+                query: {
+                  app: get('app_name'),
+                  size: size,
+                  time: {
+                    $gt: s.scan_time
+                  }
+                },
+                limit: c.scanner_limit,
+                sort: {
+                  time: 1
+                }
+              }, function (err, scan_ticks) {
+                if (err) return done(err)
+                if (!scan_ticks.length) {
+                  // fill in final gap to the present
+                  var sub_tasks = [], now = new Date().getTime()
+                  while (s.scan_time <= now) {
+                    (function (scan_time) {
+                      var tick_id = tb(scan_time).resize(size).toString()
+                      sub_tasks.push(function (sub_done) {
+                        // fill in missing ticks
+                        var missing_tick = tick_defaults(tick_id, size)
+                        g.last_tick = g.tick
+                        g.tick = missing_tick
+                        scan_tick(g, sub_done)
+                      })
+                    })(s.scan_time)
+                    s.scan_time = tb(s.scan_time).resize(size).add(1).toMilliseconds()
+                  }
+                  return series(sub_tasks, cb)
+                }
+                var index = {}, max_time = 0
+                scan_ticks.forEach(function (tick) {
+                  index['time:' + tick.time] = tick
+                  max_time = Math.max(max_time, tick.time)
+                })
+                with_index(index, max_time)
+              })
+              function with_index (index, max_time) {
+                // scan_time is incremented after each scan
+                // fill in missing ticks with default values
+                var sub_tasks = []
+                //console.error('index', index.length)
+                while (s.scan_time < max_time) {
+                  // interpolate
+                  (function (scan_time) {
+                    var current_tick = index['time:' + scan_time]
+                    if (!current_tick) {
+                      current_tick = tick_defaults(tb(scan_time).resize(size).toString(), size)
+                    }
+                    sub_tasks.push(function (sub_done) {
+                      g.last_tick = g.tick
+                      g.tick = current_tick
+                      scan_tick(g, sub_done)
+                    })
+                  })(s.scan_time)
+                  s.scan_time = tb(s.scan_time).resize(size).add(1).toMilliseconds()
+                }
+                series(sub_tasks, scan)
+              }
+            })()
+          }
+        })
+        
       }
     })
     parallel(tasks, cb)
