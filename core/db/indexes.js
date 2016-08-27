aea56ee13d4a84fc6cf6c269f6ce5257a958768c
Carlos Rodriguez <carlos@s8f.org>
2016-08-27 00:08:22 -0700

    add index with processed

--- core/db/indexes.js
+++ core/db/indexes.js
@@ -4,7 +4,7 @@ module.exports = function container (get, set) {
   return function ensure_indexes (cb) {
     var tasks = []
     tasks.push(function (done) {
-      get('db.mongo.db').collection('ticks').ensureIndex({app: 1, time: 1, size: 1}, done)
+      get('db.mongo.db').collection('ticks').ensureIndex({app: 1, time: 1, size: 1, processed: 1}, done)
     })
     tasks.push(function (done) {
       get('db.mongo.db').collection('logs').ensureIndex({app: 1, time: 1, feed: 1}, done)
